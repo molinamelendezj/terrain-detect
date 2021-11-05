91ac07609b71817c4274d4f958353163e55b396b
Jose Molina-Melendez <molinamelendezj@gmail.com>
2021-11-05 17:12:18 -0400

    Update window-slices.py

--- window-slices.py
+++ window-slices.py
@@ -7,14 +7,14 @@ from glob import glob
 
 x_stock, x_time_stock, y_stock, y_time_stock = [], [], [], []
 for subject in range(1,10):
-    x_stock.append(sorted(glob(f'/iot-readings/TrainingData/subject_00{subject}_0*__x.csv')))
-    x_time_stock.append(sorted(glob(f'/iot-readings/TrainingData/subject_00{subject}_0*__x_time.csv')))
-    y_stock.append(sorted(glob(f'/iot-readings/TrainingData/subject_00{subject}_0*__y.csv')))
-    y_time_stock.append(sorted(glob(f'/iot-readings/TrainingData/subject_00{subject}_0*__y_time.csv')))
+    x_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__x.csv')))
+    x_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__x_time.csv')))
+    y_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__y.csv')))
+    y_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__y_time.csv')))
 '''for subject in range(3):
-    x_stock.append(sorted(glob(f'/iot-readings/TrainingData/subject_01{subject}_0*__x.csv')))
-    x_time_stock.append(sorted(glob(f'/iot-readings/TrainingData/TestData/subject_01{subject}_0*__x_time.csv')))
-    y_time_stock.append(sorted(glob(f'/iot-readings/TrainingData/TestData/subject_01{subject}_0*__y_time.csv')))'''
+    x_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_01{subject}_0*__x.csv')))
+    x_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/TestData/subject_01{subject}_0*__x_time.csv')))
+    y_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/TestData/subject_01{subject}_0*__y_time.csv')))'''
 y_stock.pop()    # delete last row (it's empty)
 
 # Flatten the stockpiles (2D to 1D)
