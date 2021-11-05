ca3fa5f574d796d1c5586b009b9170dadd442e54
Jose Molina-Melendez <molinamelendezj@gmail.com>
2021-11-04 23:09:09 -0400

    Update window-slices.py

--- window-slices.py
+++ window-slices.py
@@ -1,1 +1,32 @@
 
+from glob import glob
+# First number = subject
+# Second number = run
+
+# Stockpile the data
+
+import python_pachyderm
+
+client = python_pachyderm.Client()
+
+f = client.get_file(("iot-readings", "master"), "/TrainingData/subject_001_01__x.csv")
+print(f.read()) 
+
+
+x_stock, x_time_stock, y_stock, y_time_stock = [], [], [], []
+for subject in range(1,10):
+    x_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TrainingData/subject_00{subject}_0*__x.csv')))
+    x_time_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TrainingData/subject_00{subject}_0*__x_time.csv')))
+    y_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TrainingData/subject_00{subject}_0*__y.csv')))
+    y_time_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TrainingData/subject_00{subject}_0*__y_time.csv')))
+for subject in range(3):
+    x_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TestData/subject_01{subject}_0*__x.csv')))
+    x_time_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TestData/subject_01{subject}_0*__x_time.csv')))
+    y_time_stock.append(sorted(glob(f'/content/drive/MyDrive/ECE542/proj-c/TestData/subject_01{subject}_0*__y_time.csv')))
+y_stock.pop()    # delete last row (it's empty)
+
+# Flatten the stockpiles (2D to 1D)
+x_stock = [item for sublist in x_stock for item in sublist]
+x_time_stock = [item for sublist in x_time_stock for item in sublist]
+y_stock = [item for sublist in y_stock for item in sublist]
+y_time_stock = [item for sublist in y_time_stock for item in sublist]
