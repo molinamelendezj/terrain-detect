d5420c8112d820f4d41a0d3f0483e3aa74b820a9
Jose Molina-Melendez <molinamelendezj@gmail.com>
2021-11-06 14:23:32 -0400

    Update window-slices.py

--- window-slices.py
+++ window-slices.py
@@ -27,10 +27,10 @@ for subject in range(1,10):
     x_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__x_time.csv')))
     y_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__y.csv')))
     y_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__y_time.csv')))
-'''for subject in range(3):
-    x_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_01{subject}_0*__x.csv')))
-    x_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/TestData/subject_01{subject}_0*__x_time.csv')))
-    y_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/TestData/subject_01{subject}_0*__y_time.csv')))'''
+for subject in range(3):
+    x_stock.append(sorted(glob(f'/pfs/iot-readings/subject_01{subject}_0*__x.csv')))
+    x_time_stock.append(sorted(glob(f'/pfs/iot-readings/TestData/subject_01{subject}_0*__x_time.csv')))
+    y_time_stock.append(sorted(glob(f'/pfs/iot-readings/TestData/subject_01{subject}_0*__y_time.csv')))
 y_stock.pop()    # delete last row (it's empty)
 
 # Flatten the stockpiles (2D to 1D)
@@ -146,30 +146,3 @@ print(len(x_val))
 print(len(y_val))
 
 print(len(x_test))
-
-if __name__ == "__main__":
-    print("hello world")
-    x_stock, x_time_stock, y_stock, y_time_stock = [], [], [], []
-    for subject in range(1,10):
-        x_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__x.csv')))
-        x_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__x_time.csv')))
-        y_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__y.csv')))
-        y_time_stock.append(sorted(glob(f'/pfs/iot-readings/TrainingData/subject_00{subject}_0*__y_time.csv')))
-    '''for subject in range(3):
-        x_stock.append(sorted(glob(f'/iot-readings/TrainingData/subject_01{subject}_0*__x.csv')))
-        x_time_stock.append(sorted(glob(f'/iot-readings/TrainingData/TestData/subject_01{subject}_0*__x_time.csv')))
-        y_time_stock.append(sorted(glob(f'/iot-readings/TrainingData/TestData/subject_01{subject}_0*__y_time.csv')))'''
-    y_stock.pop()    # delete last row (it's empty)
-
-    # Flatten the stockpiles (2D to 1D)
-    x_stock = [item for sublist in x_stock for item in sublist]
-    x_time_stock = [item for sublist in x_time_stock for item in sublist]
-    y_stock = [item for sublist in y_stock for item in sublist]
-    y_time_stock = [item for sublist in y_time_stock for item in sublist]
-
-    print(x_stock)
-    
-    f= open("/pfs/out/pre-process-stage/guru99.txt","w+")
-    for i in range(10):
-     f.write("This is line %d\r\n" % (i+1))
-    f.close()
